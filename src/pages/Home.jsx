d500ca9fcc6f467388195e6756095f8722d52d3c
Juan Marin <juandavidmarin.0008@gmail.com>
2026-02-20 22:19:43 -0500

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import "../styles/Home.css";
 import { useState, useEffect } from "react";
 import { Cards } from "../components/Cards";
 import { useAuth0 } from "@auth0/auth0-react";
-import ChatIA from "../components/chatAI";
+import { ChatIA } from "../components/chatAI";
 
 export function Home() {
   const { logout } = useAuth0();
