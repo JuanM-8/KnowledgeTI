6382536e216604b725ad982124e6f94244be3aa3
Juan Marin <juandavidmarin.0008@gmail.com>
2026-04-29 20:54:25 -0500

    New documentation

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -2,7 +2,7 @@ export default function Docs() {
   return (
     <div style={{ width: "100%", height: "calc(100vh - 60px)" }}>
       <iframe
-        src="manuals/KnowledgeTI_Documentacion.pdf.pdf"
+        src="manuals/documentacion-knowledgeti.pdf"
         width="100%"
         height="100%"
         style={{ border: "none" }}
